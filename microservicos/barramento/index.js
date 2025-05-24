e6990c210962440a9daac40bcb0e7e239215d8cd
ANDRE SOLANO FERREIRA RODRIGUES MAIOLINI <19.02012-0@maua.br>
2025-05-24 09:24:41 -0300

    learn(ap01-mss): encerra secao 4.3.52

--- microservicos/barramento/index.js
+++ microservicos/barramento/index.js
@@ -10,42 +10,56 @@ const lembretes_port = 4000
 const consulta_port = 6000
 const classificacao_port = 7000
 
+// Cadastro de todos os eventos
+const baseEventos = {
+    data: []
+}
+
 app.post('/eventos', async (req, res) => {
 
     // 1. Pegar o evento
     const evento = req.body
     console.log(evento)
 
-    // 2. Enviar o evento para o mss de lembretes
+    // 2. Registra novo evento
+    baseEventos['data'].push(evento)
+
+    // 3. Enviar o evento para o mss de lembretes
     try {
         await axios.post(`http://localhost:${lembretes_port}/eventos`, evento)
     } catch (e) {
         console.log(e)
     }
 
-    // 3. Enviar o evento para o mss de observações
+    // 4. Enviar o evento para o mss de observações
     try {
         await axios.post(`http://localhost:${observacoes_port}/eventos`, evento)
     } catch (e) {
         console.log(e)
     }
 
-    // 4. Enviar o evento para o mss de observações
+    // 5. Enviar o evento para o mss de observações
     try {
         await axios.post(`http://localhost:${consulta_port}/eventos`, evento)
     } catch (e) {
         console.log(e)
     }
 
-    // 5. Enviar o evento para o mss de classificacao
+    // 6. Enviar o evento para o mss de classificacao
     try {
         await axios.post(`http://localhost:${classificacao_port}/eventos`, evento)
     } catch (e) {
         console.log(e)
     }
 
-    // 6. "Responder" a requisição para o remetente
+    // 7. "Responder" a requisição para o remetente
     res.end()
+
+})
+
+app.get('/eventos', (req, res) => {
+
+    res.json(baseEventos)
 
 })
 
--- microservicos/consulta/index.js
+++ microservicos/consulta/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+const axios = require('axios')
 
 const port = 6000
 
@@ -28,17 +29,48 @@ const port = 6000
 }
 */
 
+const getBacklog = async () => {
+
+    try {
+
+        const resAxios = await axios.get('http://localhost:10000/eventos')
+        const eventos = resAxios.data
+
+        eventos.forEach(async element => {
+            const etype = element.type
+            if (etype === LembreteCriado || etype === ObservacaoCriada || etype === ObservacaoAtualizada) {
+                
+                try {
+                    await funcoes[etype](element.payload)
+                } catch (e) {
+                    console.log(e)
+                }
+                
+            }
+        });
+
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
 const baseConsolidada = { data: [] }
 
 const funcoes = {
-    LembreteCriado: (lembrete) => {
+    LembreteCriado: async (lembrete) => {
         baseConsolidada.data[lembrete.id - 1] = baseConsolidada.data[lembrete.id - 1] || { observacoes: [] }
         Object.assign(baseConsolidada.data[lembrete.id - 1], lembrete)
     },
-    ObservacaoCriada: (observacao) => {
+    ObservacaoCriada: async (observacao) => {
         const observacoes = baseConsolidada.data[observacao.lembreteId - 1]['observacoes'] || []
         observacoes.push(observacao)
         baseConsolidada.data[observacao.lembreteId - 1]['observacoes'] = observacoes
+    },
+    ObservacaoAtualizada: async (observacao) => {
+        const observacoes = baseConsolidada.data[observacao.lembreteId - 1]['observacoes'] || []
+        const observacao_index = observacoes.findIndex((obs) => { obs.id === observacao.id })
+        observacoes[observacao_index] = observacao;
     }
 }
 
@@ -51,18 +83,29 @@ app.get('/lembretes', (req, res) => {
 app.post('/eventos', async (req, res) => {
 
 
-    const evento = req.body
+    try {
 
-    console.log(evento)
+        const evento = req.body
 
-    funcoes[evento.type](evento.payload)
+        console.log(evento)
 
-    res.end()
+        funcoes[evento.type](evento.payload)
+
+    } catch (e) {
+
+        console.log(e)
+
+    } finally {
+
+        res.end()
+
+    }
 
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
 
+    getBacklog()
     console.log(`Consulta (${port}): [OK]`)
 
 })
